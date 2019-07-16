ca75692e243aa2764ab06efeeccd5d017e639b02
akshaykalyanashetti <akshaykalyanashetti@gmail.com>
2019-07-16 15:41:21 +0530

    updated

--- Pages/task.py
+++ Pages/task.py
@@ -3,8 +3,8 @@ class TaskScreen:
     def __init__(self,driver):
         WebGeneric.__init__(self, driver)
         self.driver = driver
-        self.user_path = '//input[@class="inputFieldWithPlaceholder newNameField inputNameField"]'
-        self.descrip ="//textarea[@placeholder='Enter Customer Description']"
+        self.user_name = '//input[@class="inputFieldWithPlaceholder newNameField inputNameField"]'
+        self.user_descrip = "//textarea[@placeholder='Enter Customer Description']"
 
 
     def task(self):
@@ -12,9 +12,9 @@ class TaskScreen:
         wg.submit('//*[@id="topnav"]/tbody/tr[1]/td[3]/a/div[1]')
         wg.submit('//div[text()="Add New"]')
         wg.submit("//div[text()='+ New Customer']")
-        wg.text(self.user_path, 'dfghsssjkaa1')
-        wg.text(self.descrip, 'good one11')
+        wg.text(self.user_name, 'Akshay121')
+        wg.text(self.user_descrip, 'good one11')
         wg.submit("//div[@class='emptySelection']")
         wg.submit('//*[@id="customerLightBox_content"]/div[3]/div[2]/div[2]')
         import time
-        time.sleep(5) #manually click on OK button
+        time.sleep(5) #manually click on OK button when javascript pop-up comes on the screen
